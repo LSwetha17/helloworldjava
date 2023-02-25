988620e15c57edf2fcfd55f617e012a775bf3649
Swetha Lingutla <125542502+LSwetha17@users.noreply.github.com>
2023-02-25 15:46:26 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - Feb 25  - 21:04 PM - Triggered by webhook");
+    System.out.println("Hello World - Feb 25  - 15:46 PM - Triggered by webhook");
     }
 }
